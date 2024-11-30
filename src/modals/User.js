8ed4cce55958bf1d3af0125fa171ff18e47cac5d
ayushsharma1100 <asayushsharma1111@gmail.com>
2024-11-30 21:01:57 +0530

    user modal

---
+++ src/modals/User.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+let UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
